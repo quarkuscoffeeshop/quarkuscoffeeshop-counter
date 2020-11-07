426a1492b7098b981abb5383d4abfbfbc2e84c6e
Jeremy Davis <jeremyrdavis@mac.com>
2020-11-07 07:54:14 -0500

    creating Receipts tables

---
+++ src/main/java/io/quarkuscoffeeshop/counter/domain/Receipt.java
@@ -0,0 +1,66 @@
+package io.quarkuscoffeeshop.counter.domain;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+@Entity @Table(name = "receipts")
+public class Receipt extends PanacheEntity {
+
+    private BigDecimal total;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<ReceiptLineItem> lineItems;
+
+    public Receipt(BigDecimal total, List<ReceiptLineItem> lineItems) {
+        this.total = total;
+        this.lineItems = lineItems;
+    }
+
+    public Receipt() {
+
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Receipt.class.getSimpleName() + "[", "]")
+                .add("total=" + total)
+                .add("lineItems=" + lineItems)
+                .add("id=" + id)
+                .toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Receipt receipt = (Receipt) o;
+        return Objects.equals(total, receipt.total) &&
+                Objects.equals(lineItems, receipt.lineItems);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(total, lineItems);
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public List<ReceiptLineItem> getLineItems() {
+        return lineItems;
+    }
+
+    public void setLineItems(List<ReceiptLineItem> lineItems) {
+        this.lineItems = lineItems;
+    }
+}
---
+++ src/main/java/io/quarkuscoffeeshop/counter/domain/ReceiptLineItem.java
@@ -0,0 +1,78 @@
+package io.quarkuscoffeeshop.counter.domain;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkuscoffeeshop.domain.Item;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+@Entity @Table(name = "line_items")
+public class ReceiptLineItem extends PanacheEntity {
+
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Receipt receipt;
+
+    private Item item;
+
+    private String name;
+
+    public ReceiptLineItem() {
+    }
+
+    public ReceiptLineItem(Receipt receipt, Item item, String name) {
+        this.receipt = receipt;
+        this.item = item;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", ReceiptLineItem.class.getSimpleName() + "[", "]")
+                .add("receipt=" + receipt)
+                .add("item=" + item)
+                .add("name='" + name + "'")
+                .add("id=" + id)
+                .toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ReceiptLineItem that = (ReceiptLineItem) o;
+        return Objects.equals(receipt, that.receipt) &&
+                item == that.item &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(receipt, item, name);
+    }
+
+    public Receipt getReceipt() {
+        return receipt;
+    }
+
+    public void setReceipt(Receipt receipt) {
+        this.receipt = receipt;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
