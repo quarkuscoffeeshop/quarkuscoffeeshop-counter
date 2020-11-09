77adc37d48c78faeb3a7bc752e489e46bbd008bf
Jeremy Davis <jeremyrdavis@mac.com>
2020-11-09 14:34:51 -0500

    persisting events

--- src/main/java/io/quarkuscoffeeshop/infrastructure/OrderRepository.java
+++ src/main/java/io/quarkuscoffeeshop/infrastructure/OrderRepository.java
@@ -5,6 +5,8 @@ import io.quarkuscoffeeshop.counter.domain.Order;
 import io.quarkuscoffeeshop.counter.domain.OrderCreatedEvent;
 import io.quarkuscoffeeshop.counter.domain.OrderEvent;
 import io.quarkuscoffeeshop.domain.EventType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.json.Json;
@@ -12,7 +14,10 @@ import javax.json.Json;
 @ApplicationScoped
 public class OrderRepository implements PanacheMongoRepository<OrderEvent> {
 
+    Logger logger = LoggerFactory.getLogger(OrderRepository.class);
+
     public void persist(final OrderCreatedEvent orderCreatedEvent) {
+        logger.debug("persisting event payload {}", JsonUtil.toJson(orderCreatedEvent));
         OrderEvent orderEvent = new OrderEvent(
                 EventType.ORDER_PLACED,
                 JsonUtil.toJson(orderCreatedEvent));
