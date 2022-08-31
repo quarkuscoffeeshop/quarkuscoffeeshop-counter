a4b5d171c1dd72b09de71fa1def03ea53a8dcc89
jeremyrdavis <jeremy.davis@redhat.com>
2022-08-31 09:07:47 -0400

    removed conflict marker

--- src/main/java/io/quarkuscoffeeshop/counter/domain/OrderRepository.java
+++ src/main/java/io/quarkuscoffeeshop/counter/domain/OrderRepository.java
@@ -8,13 +8,8 @@ import javax.enterprise.context.ApplicationScoped;
 public class OrderRepository implements PanacheRepository<OrderRecord> {
 
     public Order findById(final String orderId) {
-<<<<<<< HEAD
-
-        return Order.findById(orderId);
-=======
         OrderRecord orderRecord = OrderRecord.findById(orderId);
         return Order.fromOrderRecord(orderRecord);
->>>>>>> c06077d8b2fd9c4db6c101e00c11ad3b879eb629
     }
 
     public void persist(Order order) {
